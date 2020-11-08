343b387623ac301d6cd811995c207f2a5e1cff21
Jason Ours <jasonours@outlook.com>
2020-11-08 14:44:23 -0600

    completed

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,13 +2,27 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  
+  constructor() {
+    super();
+    this.state = { 
+      plants: []
+    };
+  }
 
-  // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    axios
+    .get("http://localhost:3333/plants")
+    .then(res => {
+      this.setState({
+        plants: res.data.plantsData
+      });
+    console.log(this.state.plants)
+    })
+    .catch(err => console.log("There is an error.", err))
+  }
 
-  /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+
   render() {
     return (
       <main className="plant-list">
@@ -21,8 +35,8 @@ export default class PlantList extends Component {
               <p>{plant.description}</p>
               <div className="plant-bottom-row">
                 <p>${plant.price}</p>
-                <p>☀️ {plant.light}</p>
-                <p>💦 {plant.watering}x/month</p>
+                <p><span role="img" aria-label="sunshine">☀️</span> {plant.light}</p>
+                <p><span role="img" aria-label="water">💦</span> {plant.watering}x/month</p>
               </div>
               <button
                 className="plant-button"
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,21 @@
-// write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = initialState => {
+    const [values, setValues] = useState(initialState);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    const handleChange = event => {
+        setValues({...values, [event.target.name]: event.target.value});
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setShowSuccessMessage(true);
+    };
+
+    const clearForm = () => {
+        setValues(initialState);
+    };
+
+    return[values, showSuccessMessage, handleChange, handleSubmit, clearForm]
+};
